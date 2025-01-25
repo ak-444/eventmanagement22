649aa4ab24bb24524c6a9d9fa5141c99496ae0c0
Hanslozano <132995316+Hanslozano@users.noreply.github.com>
2025-01-25 17:08:07 +0800

    register.php

--- register.php
+++ register.php
@@ -25,20 +25,25 @@
 </head>
 <body>
   <div class="login-container"> 
+
     <h2 class="text-center mb-4">Register</h2>
-    <form action="process_register.php" method="POST">
+    <form action="process_register.php" method="POST"  autocomplete="off">
+
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
         <input type="text" class="form-control" id="name" name="name" required>
       </div>
+
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" required>
+        <input type="email" class="form-control" id="email" name="email"  autocomplete="off" required>
       </div>
+
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password" required>
+        <input type="password" class="form-control" id="password"  autocomplete="new-password" name="password" required>
       </div>
+
       <div class="mb-3">
         <label for="user_type" class="form-label">User Type</label>
         <select class="form-select" id="user_type" name="user_type" required>
@@ -47,7 +52,17 @@
           <option value="Faculty">Faculty</option>
         </select>
       </div>
+
+
       <button type="submit" class="btn btn-primary w-100">Register</button>
+
+      
+      <?php if (isset($_GET['success']) && $_GET['success'] == 1): ?>
+    <div class="alert alert-success mt-4" role="alert">
+        Registration successfully completed! <a href="login.php" class="btn btn-link">Proceed to Login</a>
+    </div>
+<?php endif; ?>
+
     </form>
   </div>
 </body>
